a2bc1ff08303c3904b368ee6922f22354cef2269
pacocoursey <pvco.coursey@gmail.com>
2019-04-08 00:52:52 -0600

    List functionality to add/remove, not saving state yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-const { render, html } = require('htm/preact');
+const { Component, render, html } = require('htm/preact');
 const Row = require('./components/Row').default;
+const Footer = require('./components/Footer').default;
 
-const App = () => html`
-  <div class="app">
-    <${Row} name="Some Name" />
-    <${Row} name="Other Longer Name" />
-  </div>
-`;
+let i = 0;
+const uniq = () => `u${i++}`;
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: {},
+    };
+  }
+
+  addRow() {
+    const key = uniq();
+
+    this.setState(prevState => ({
+      list: {
+        ...prevState.list,
+        [key]: {},
+      },
+    }));
+  }
+
+  removeRow(key) {
+    const { list } = this.state;
+
+    delete list[key];
+
+    this.setState({
+      list,
+    });
+  }
+
+  render() {
+    const { list } = this.state;
+
+    return html`
+      <div class="app">
+        <div class="list">
+          ${Object.keys(list).map(key => html`
+            <${Row}
+              name=${list[key].name}
+              key=${key}
+              name=${key}
+              onremove=${() => { this.removeRow(key); }}
+            />
+          `)}
+        </div>
+
+        <${Footer} onclick=${() => { this.addRow(); }} />
+      </div>
+    `;
+  }
+}
 
 const container = document.querySelector('.container');
 render(html`<${App} />`, container);
--- src/components/Row.js
+++ src/components/Row.js
@@ -30,13 +30,14 @@ class Row extends Component {
   }
 
   render() {
-    const { name } = this.props;
+    const { name, key, onremove } = this.props;
     const { choice } = this.state;
 
     return html`
       <div class="row">
         <${Checkbox}
           onchange=${() => { console.log('eee'); }}
+          id=${key}
           checked
         />
         <div>${name}</div>
@@ -45,6 +46,12 @@ class Row extends Component {
           onchange=${e => this.handleChange(e)}
         />
         ${choice === 'click' || choice === 'right click' ? html`<${Input} hide />` : html`<${Input} />`}
+        <div
+          class="remove"
+          onclick=${onremove}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+        </div>
       </div>
     `;
   }
